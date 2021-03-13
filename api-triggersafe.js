008998de30ee7bffbbc77040ea48e5c9c0dab789
ProjeXCode <24662855+ProjeXCode@users.noreply.github.com>
2021-03-14 00:04:47 +0700

    Update api-triggersafe.js

--- api-triggersafe.js
+++ api-triggersafe.js
@@ -5,6 +5,6 @@ $(document).ready(function(){
     idsafe[2]='3435246800074833531';
     idsafe[3]='3435246800074833531';
 var getrandomlink = Math.round(Math.random() * 3);
-var geturl ="https://www.blogger.com/feeds/" + idsafe[getrandomlink] + "/posts/default/-/News?alt=json-in-script&max-results=150&callback=showurl" ;
+var geturl ="https://www.blogger.com/feeds/" + idsafe[getrandomlink] + "/posts/default?alt=json-in-script&max-results=150&callback=showurl" ;
 location.href = geturl;
 });
